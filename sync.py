51b4b1efbf7d4b2b5cedb39f851ad40d62cd0cb4
Sebastian Pech <sebastian.pech@me.com>
2021-01-12 12:08:06 +0100

    Add initial version

---
+++ sync.py
@@ -0,0 +1,67 @@
+import os
+import subprocess
+import argparse
+import pathlib
+
+parser = argparse.ArgumentParser(description='Synchronize reMarkable and Zotero tablet export.')
+parser.add_argument('Zotero', help="Basefolder defined in ZotFile tablet settings.")
+parser.add_argument('reMarkable', help="Mirrored folder on reMarkable.")
+parser.add_argument('--delete', action='store_true', help="Delete all files on reMarkable that don't exists in the Zotero folder.")
+
+args = parser.parse_args()
+
+ZOTERO_FOLDER = args.Zotero
+RM_FOLDER =  args.reMarkable
+
+RMAPI_BIN = os.path.join(os.path.dirname(os.path.abspath(__file__)),"bin","rmapi")
+
+def rmapi(cmd):
+    return subprocess.check_output(f"{RMAPI_BIN} {cmd}", shell=True, stderr=subprocess.STDOUT).decode("utf-8").split('\n')[0:-1]
+
+def upload_file(file):
+    path = os.path.join(ZOTERO_FOLDER, f"{file}.pdf")
+    rmapi(f'put "{path}" "{RM_FOLDER}"')
+
+def download_file(file):
+    path = '/'.join([RM_FOLDER,file])
+    zip_file = f"{file}.zip"
+    try:
+        rmapi(f'geta -a "{path}"')
+        f_annotations = f"{file}-annotations.pdf"
+        os.replace(f_annotations,os.path.join(ZOTERO_FOLDER,f"{file}.pdf"))
+    except Exception as inst:
+        if not ("document has no pages" in inst.output.decode("utf-8")):
+            print(inst.output.decode("utf-8"))
+    finally:
+        if (os.path.isfile(zip_file)):
+            os.remove(zip_file)
+
+def delete_file(file):
+    path = '/'.join([RM_FOLDER,file])
+    rmapi(f'rm "{path}"')
+
+def get_files():
+    files_on_remarkable = set([f.split('\t')[-1] for f in rmapi(f"ls {RM_FOLDER}")])
+    files_on_local = set([os.path.splitext(os.path.basename(f))[0] for f in os.listdir(ZOTERO_FOLDER)])
+    return files_on_remarkable, files_on_local
+
+def process_files(delete=False):
+    files_on_remarkable, files_on_local = get_files()
+    files_to_download = files_on_remarkable & files_on_local
+    files_to_upload = files_on_local - files_on_remarkable
+    files_to_delete = files_on_remarkable - files_on_local
+
+    for file in files_to_upload:
+        print(f"[Uploading  ] '{file}'")
+        upload_file(file)
+
+    for file in files_to_download:
+        print(f"[Downloading] '{file}'")
+        download_file(file)
+
+    if delete:
+        for file in files_to_delete:
+            print(f"[Deleting   ] '{file}'")
+            delete_file(file)
+
+process_files(delete=args.delete)
